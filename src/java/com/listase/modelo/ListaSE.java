e2f701dbfea05d14d8a94c97ff5dde9895ac1edf
Carlos Loaiza <carlos.loaiza@pragmaticingenieria.com>
2020-03-21 09:34:46 -0500

    Codificación modelo lista simplemente enlazada

---
+++ src/java/com/listase/modelo/ListaSE.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.listase.modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author carloaiza
+ */
+public class ListaSE implements Serializable{
+    private Nodo cabeza;
+
+    public ListaSE() {
+    }
+
+    public Nodo getCabeza() {
+        return cabeza;
+    }
+
+    public void setCabeza(Nodo cabeza) {
+        this.cabeza = cabeza;
+    }
+    
+    public void adicionarNodo(Infante infante)
+    {
+        if(cabeza ==null)
+        {
+            cabeza = new Nodo(infante);
+        }
+        else
+        {
+            //Lamo a mi ayudante
+            Nodo temp= cabeza;
+            while(temp.getSiguiente()!=null) //Mientras que en siguiente exista algo
+            {
+                temp= temp.getSiguiente();
+            }
+            //temp va estar ubicado en el ultimo nodo
+            temp.setSiguiente(new Nodo(infante));
+        }
+        
+    }
+    
+    public void adicionarNodoAlInicio(Infante infante)
+    {
+        if(cabeza ==null)
+        {
+            cabeza = new Nodo(infante);
+        }
+        else
+        {
+            Nodo temp= new Nodo(infante);
+            temp.setSiguiente(cabeza);
+            cabeza= temp;
+        }
+    }
+    
+    public short contarNodos()
+    {
+        if(cabeza ==null)
+        {
+            return 0;
+        }
+        else
+        {
+            //llamar a mi ayudante
+            Nodo temp= cabeza;
+            short cont=1;
+            while(temp.getSiguiente()!=null)
+            {
+                temp=temp.getSiguiente();
+                cont++;
+            }
+            return cont;
+        }
+    }
+    
+    
+    
+    
+}
